1ef7cd24aab81ba708d568311ae0c2f21fda4a17
FishManHell <56148058+FishManHell@users.noreply.github.com>
2020-06-05 10:04:32 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var userName =prompt('Please enter your name');
+
+var age =prompt('Please enter your age');
+
+var year = 2020;
+
+year = year - age;
+
+console.log(userName + ' ' + 'was born in' + ' ' + year);
